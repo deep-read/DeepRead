edaf174aceed977fff6d2fd6b77157def5122891
Harpal Sahota <harpal28sahota@gmail.com>
2019-07-16 17:25:34 +0100

    changed paths

--- deep_read/yolo_v3/scripts/create_training_file.py
+++ deep_read/yolo_v3/scripts/create_training_file.py
@@ -7,11 +7,11 @@ import numpy as np
 
 np.random.seed(42)
 
-DATA_DIR = Path(f'{os.getcwd()}/data/object_detection/smart_new_homes/iteration_1/processed')
-CONFIG_DIR = Path('./cv_training_stack/object_detection/smart_new_homes/data_config')
+DATA_DIR = Path(f'{os.getcwd()}\data')
+CONFIG_DIR = Path('.\data\model_config')
 
-object_files = os.listdir(DATA_DIR/'image_objects')
-image_files = os.listdir(DATA_DIR/'images')
+object_files = os.listdir(DATA_DIR/'labelled_images')
+image_files = os.listdir(DATA_DIR/'book_covers')
 
 with open(CONFIG_DIR/'train.txt', 'w') as out_file, open(CONFIG_DIR/'valid.txt', 'w') as valid_file:
     for f in object_files:
@@ -21,11 +21,9 @@ with open(CONFIG_DIR/'train.txt', 'w') as out_file, open(CONFIG_DIR/'valid.txt',
                 if f in image:
                     image_name = image
                     if np.random.sample() > 0.1:
-                        # out_file.write(f'{DATA_DIR}/all_images/{image_name}\n')
-                        out_file.write(f'data/custom/images/{image_name}\n')
+                        out_file.write(f'{DATA_DIR}\\book_covers\\{image_name}\n')
                     else:
-                        # valid_file.write(f'{DATA_DIR}/all_images/{image_name}\n')
-                        valid_file.write(f'data/custom/images/{image_name}\n')
+                        valid_file.write(f'{DATA_DIR}\\book_covers\\{image_name}\n')
                     break
             else:
                 print(f'Image not found: {f}')
